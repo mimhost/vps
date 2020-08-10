ed1b21d7b9011b3467e356475a4adf8b78fa4ba1
Apeachsan91 <30749674+Apeachsan91@users.noreply.github.com>
2020-08-10 17:29:06 +0800

    Update and rename install to shin.sh

--- shin.sh
+++ shin.sh
@@ -1237,4 +1237,4 @@ echo "---------------------------- SILA REBOOT VPS ANDA! -----------------------
 
  # Clearing all logs from installation
 rm -rf /root/.bash_history && history -c && echo '' > /var/log/syslog
-rm -f install*
+rm -f shin.sh*
